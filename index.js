b28eca5e633fc90463ae70af5a7520a366e1baa8
Blessing Awak <melissasignature1@gmail.com>
2024-02-24 18:26:17 +0100

    commit

--- index.js
+++ index.js
@@ -167,7 +167,10 @@ for (const [i, player] of game.scored.entries())
   console.log(`Goal ${i + 1}: ${player}`);
 
 //2.
+const odds = Object.values(game.odds);
 let average = 0;
-for (const odd of Object.values(game.odds)) average += odd;
-average /= Object.values(game.odds);
+for (const odd of odds) average += odd;
+average /= odds.length;
 console.log(average);
+
+//3.
