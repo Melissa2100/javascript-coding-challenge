1a807cdb118580d20ba614778fc25dacdcf96f48
Blessing Awak <melissasignature1@gmail.com>
2024-02-24 18:20:22 +0100

    commit

--- index.js
+++ index.js
@@ -111,7 +111,7 @@
     //Odd of victory Bayern Munich: 1.33
     //Odd of draw: 3.25
     //Odd of victory Borrussia Dortmund: 6.5
-//Get the team names directly from the game object, don't hardcode theM (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+//Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
 // 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value,In this game, it will look like this:
 // {
 //  Gnarby: 1,
@@ -162,4 +162,10 @@ const game = {
 };
 
 //SOLUTION
+// 1. 
+for(const [i, player] of game.scored.entries())
+      console.log(`Goal ${i + 1}: ${player}`);
+
+//2.
+
 
