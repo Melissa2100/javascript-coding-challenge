6ff74781f06c4e4c12ab215282dd9403e32350a1
Blessing Awak <melissasignature1@gmail.com>
2024-02-24 18:10:38 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// CODING CHALLENGE
+// Data structures, modern operators, and strings
+// CODING CHALLENGE 1
 // We're building a football betting app (soccer for my American friends �)!
 // Suppose we get data from a web service about a certain game ('game' variable on
 // next page). In this challenge we're gonna work with that data.
@@ -25,74 +26,96 @@
 // Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
 // Then, call the function again with players from game.scored
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandoski", "Hummels"],
-  date: "Feb 7th, 2024",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandoski", "Hummels"],
+//   date: "Feb 7th, 2024",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-// solutions
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// // solutions
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
 
-// 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// // 3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-// 4.
-const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
+// // 4.
+// const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
 
-// 5.
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
+// // 5.
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
 
-// 6.
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
+// // 6.
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
 
-printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
-printGoals("Davies", "Muller");
-printGoals(game.scored);
+// // printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// // printGoals("Davies", "Muller");
+// printGoals(...game.scored);
+
+// // 7.
+// team1 < team2 && console.log("Team 1 is more likely to win");
+// team1 > team2 && console.log("Team 2 is more likely to win");
+
+// CODING CHALLENGE 2
+// Let's continue with our football betting app! Keep using the 'game' variable from before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+    //Odd of victory Bayern Munich: 1.33
+    //Odd of draw: 3.25
+    //Odd of victory Borrussia Dortmund: 6.5
+//Get the team names directly from the game object, don't hardcode theM (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+// 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value,In this game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
+// }
+// GOOD LUCK 😄
